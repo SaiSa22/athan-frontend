9a241572f89a685c28505c8ac59ffff3f67ec6bf
SaiSa22 <ssabti@gmail.com>
2026-01-18 12:32:37 -0600

    Fix supabaseUrl declaration syntax

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // REPLACE WITH YOUR SUPABASE URL AND ANON KEY
-const supabaseUrl = 'https://qwifaauejfmhuhyyjius.supabase.co'
+const supabaseUrl = 'https://qwifaauejfmhuhyyjius.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3aWZhYXVlamZtaHVoeXlqaXVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxMDE1MTYsImV4cCI6MjA4MTY3NzUxNn0.sm0OYygWgRPUvUuyb3R1lC8KXD0C9l1tJ5PduUHHzOU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
