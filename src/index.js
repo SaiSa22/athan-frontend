06302fef1468e90c534fb580bee513c7a561c305
SaiSa22 <ssabti@gmail.com>
2026-01-17 22:12:50 -0600

    Remove App.css import from index.js
    Removed the import statement for App.css.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.css';
 import App from './App';
 
 // This finds the <div> with id="root" in your HTML and puts your App inside it
