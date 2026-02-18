f2572a7f0d14dce01a6a29355d80f71114a5488b
SaiSa22 <ssabti@gmail.com>
2026-02-17 23:48:47 -0600

    Refactor CALCULATION_METHODS by removing duplicates
    Removed duplicate entries and unnecessary options from CALCULATION_METHODS.

--- src/pages/DeviceManager.jsx
+++ src/pages/DeviceManager.jsx
@@ -16,27 +16,17 @@ const s3Client = new S3Client({
 });
 
 const CALCULATION_METHODS = [
+  { id: 0, name: "Jafari / Shia Ithna-Ashari" },
+  { id: 7, name: "Institute of Geophysics, University of Tehran" },
   { id: 2, name: "Islamic Society of North America (ISNA)" },
   { id: 3, name: "Muslim World League" },
   { id: 4, name: "Umm Al-Qura University, Makkah" },
-  { id: 5, name: "Egyptian General Authority of Survey" },
-  { id: 1, name: "University of Islamic Sciences, Karachi" },
-  { id: 0, name: "Jafari / Shia Ithna-Ashari" },
-  { id: 7, name: "Institute of Geophysics, University of Tehran" },
   { id: 8, name: "Gulf Region" },
-  { id: 9, name: "Kuwait" },
   { id: 10, name: "Qatar" },
-  { id: 11, name: "Majlis Ugama Islam Singapura, Singapore" },
-  { id: 12, name: "Union Organization islamic de France" },
   { id: 13, name: "Diyanet İşleri Başkanlığı, Turkey" },
   { id: 14, name: "Spiritual Administration of Muslims of Russia" },
   { id: 15, name: "Moonsighting Committee Worldwide" },
-  { id: 16, name: "Dubai (experimental)" },
   { id: 17, name: "Jabatan Kemajuan Islam Malaysia (JAKIM)" },
-  { id: 18, name: "Tunisia" },
-  { id: 19, name: "Algeria" },
-  { id: 20, name: "KEMENAG - Indonesia" },
-  { id: 21, name: "Morocco" },
   { id: 22, name: "Comunidade Islamica de Lisboa" },
   { id: 23, name: "Ministry of Awqaf, Jordan" }
 ];
