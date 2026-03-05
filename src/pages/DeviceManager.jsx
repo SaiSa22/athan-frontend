652c4ca3bf23f7317115650847932c7ea427e18e
SaiSa22 <ssabti@gmail.com>
2026-03-04 21:34:43 -0600

    Add Mohammad Zadeh to audio options

--- src/pages/DeviceManager.jsx
+++ src/pages/DeviceManager.jsx
@@ -37,6 +37,7 @@ const CALCULATION_METHODS = [
 const AUDIO_OPTIONS = [
   { name: "Kazem Zadeh", filename: "Kazem-Zadeh.mp3" },
   { name: "Rezaeian", filename: "Rezaeian.mp3" },
+   { name: "Mohammad Zadeh", filename: "Mohammad-Zadeh.mp3" },
   { name: "Abdul Basit", filename: "Abdul-Basit.mp3" },
   { name: "Minshawi", filename: "Minshawi.mp3" },
   { name: "Al-Aqsa", filename: "Adhan-Alaqsa.mp3" },
